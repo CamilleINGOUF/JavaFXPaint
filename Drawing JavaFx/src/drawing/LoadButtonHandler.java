cc4afbbc52f820f3da1bfd83cfe8e957bf0921b8
CamilleINGOUF <ingouf.camille@gmail.com>
2017-12-19 10:02:19 +0100

    show file chooser when loading file

--- Drawing JavaFx/src/drawing/LoadButtonHandler.java
+++ Drawing JavaFx/src/drawing/LoadButtonHandler.java
@@ -2,7 +2,8 @@ package drawing;
 
 import java.io.FileNotFoundException;
 
-import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.stream.XMLStreamException;
 
 import javafx.event.ActionEvent;
@@ -18,16 +19,19 @@ public class LoadButtonHandler implements EventHandler<ActionEvent> {
 
 	@Override
 	public void handle(ActionEvent event) {
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter(
+				"xml files (*.xml)", "xml");
+		chooser.setDialogTitle("Open schedule file");
+		chooser.setFileFilter(xmlfilter);
+		chooser.showOpenDialog(null);
 		try {
-			String s = JOptionPane.showInputDialog("File name ?");
-			if(s == null || s.equals(""))
-				return;
-		
-			XmlUtilLoad.loadDrawing(drawing,s+".xml");
-		} catch (FileNotFoundException | XMLStreamException e) {
+			XmlUtilLoad.loadDrawing(drawing,chooser.getSelectedFile().getName());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
